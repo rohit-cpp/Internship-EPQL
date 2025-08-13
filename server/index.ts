8cae79317813603f7fa1db251a409a19e7ead3ac
Rohit <rrgawanderohit@gmail.com>
2025-08-13 12:25:13 +0530

    imported env file in index

--- server/index.ts
+++ server/index.ts
@@ -20,7 +20,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin:process.env.FRONTEND_URL,
     credentials:true
 }
 app.use(cors(corsOptions));
